35237df5103ae690d4538c4ca82fb55ba5a1b942
Jamison Dance <jergason@gmail.com>
2014-12-25 16:36:53 -0500

    intial bullets

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 function startGame(width, height, tanks) {
+  console.log('width is', width, 'height is', height);
   var state = {
     width: width,
     height: height,
@@ -29,24 +30,48 @@ function initRandomTank(width, height) {
 
 // a dummy default ai to do nothing to change the params
 function defaultAi(state, tank) {
-  return {velocity: tank.velocity, rotation: tank.rotation};
+  return {velocity: tank.velocity, rotation: tank.rotation, shoot: true};
 }
 
-var i = 0;
 // given a game state and array of tank ais, return the newly updated game state
 function tick(gameState) {
   gameState.tanks = gameState.tanks.map(updateTankPosition(gameState.width, gameState.height));
+  gameState.bullets = gameState.bullets.map(updateBulletPosition).filter(isInsideBounds(gameState.width, gameState.height));
 
-  if (i % 100 == 0) {
-    //console.log('ticking', gameState);
-  }
-  i++;
-  //gameState.bullets = gameState.bullets.map(updateBulletPosition);
+  // TODO:
+  // 0. allow tanks to shoot
+  // 0. check for collisions between bullets and tanks
+  // How to handle bullets that pass through tanks? draw a line, check if line
+  // goes through tanks?
+  // 0. if collided, remove health and optionally remove tank
   return gameState;
 }
 
 function constrain(value, maxValue) {
-  return Math.min(800, Math.max(value, 0));
+  return Math.min(maxValue, Math.max(value, 0));
+}
+
+function isInsideBounds(maxX, maxY) {
+  return function insideBounds(item) {
+    return (item.x >= 0 && item.x <= maxX && item.y >= 0 && item.y <= maxY);
+  }
+}
+
+var BULLET_VELOCITY = 10;
+function updateBulletPosition(bullet) {
+  return {
+    x: getX(bullet.rotation, BULLET_VELOCITY, bullet.x),
+    y: getY(bullet.rotation, BULLET_VELOCITY, bullet.y),
+    rotation: bullet.rotation
+  };
+}
+
+function getX(rotation, velocity, x) {
+  return x + Math.cos(rotation) * velocity;
+}
+
+function getY(rotation, velocity, y) {
+  return y + Math.sin(rotation) * velocity;
 }
 
 
@@ -55,9 +80,8 @@ function constrain(value, maxValue) {
  */
 function updateTankPosition(maxX, maxY) {
   return function tankMap(tank) {
-    var newX = constrain(tank.x + Math.cos(tank.rotation) * tank.velocity);
-    // flip y because coords start at 0,0 in the top left
-    var newY = constrain(tank.y +   Math.sin(tank.rotation) * tank.velocity);
+    var newX = constrain(getX(tank.x, tank.rotation, tank.velocity), maxX);
+    var newY = constrain(getY(tank.y, tank.rotation, tank.velocity), maxY);
 
     return {
       velocity: tank.velocity,
